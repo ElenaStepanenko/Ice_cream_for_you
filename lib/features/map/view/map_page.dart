cc6bf073afc1f73e99506ca250b8123aec01549e
Energy00 <newenglishmen.98@gmail.com>
2023-04-05 17:53:54 +0700

    map_page

--- lib/features/map/view/map_page.dart
+++ lib/features/map/view/map_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'dart:math' as math;
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -11,7 +13,6 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
@@ -19,71 +20,67 @@ class _MapPageState extends State<MapPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: 15.0),
-                      width: 270,
-                      //color: Colors.white,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.4),
-                            spreadRadius: 5,
-                            blurRadius: 30,
-                            offset: const Offset(
-                                0, 10), // changes position of shadow
-                          ),
-                        ],
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  width: 270,
+                  //color: Colors.white,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.4),
+                        spreadRadius: 5,
+                        blurRadius: 30,
+                        offset:
+                            const Offset(0, 10), // changes position of shadow
                       ),
+                    ],
+                  ),
 
-                      child: const TextField(
-                        decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.search_rounded,
-                            size: 30.0,
-                          ),
-                          hintText: "Поиск",
-                          //fillColor: Colors.white,
-                          //filled: true,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.search_rounded,
+                        size: 30.0,
+                      ),
+                      hintText: "Поиск",
+                      //fillColor: Colors.white,
+                      //filled: true,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
                       ),
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 16,
-                            offset: const Offset(
-                                0, 10), // changes position of shadow
-                          ),
-                        ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 16,
+                        offset:
+                            const Offset(0, 10), // changes position of shadow
                       ),
-                      child: const CircleAvatar(
-                        radius: 30.0,
-                        backgroundColor:
-                            Color.fromRGBO(108, 62, 126, 1.0),
-                        child: IconButton(
-                          icon: Icon(
-                            Icons.favorite,
-                            size: 30.0,
-                            color: Color.fromRGBO(255, 255, 255, 1.0),
-                          ),
-                          onPressed: null,
-                        ),
+                    ],
+                  ),
+                  child: const CircleAvatar(
+                    radius: 30.0,
+                    backgroundColor: Color.fromRGBO(108, 62, 126, 1.0),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.favorite,
+                        size: 30.0,
+                        color: Color.fromRGBO(255, 255, 255, 1.0),
                       ),
-                    )
-                  ]),
+                      onPressed: null,
+                    ),
+                  ),
+                )
+              ]),
               Row(mainAxisAlignment: MainAxisAlignment.end, children: [
                 Container(
                   decoration: BoxDecoration(
@@ -93,15 +90,14 @@ class _MapPageState extends State<MapPage> {
                         color: Colors.grey.withOpacity(0.5),
                         spreadRadius: 5,
                         blurRadius: 16,
-                        offset: const Offset(
-                            0, 10), // changes position of shadow
+                        offset:
+                            const Offset(0, 10), // changes position of shadow
                       ),
                     ],
                   ),
                   child: CircleAvatar(
                     radius: 30.0,
-                    backgroundColor:
-                        const Color.fromRGBO(108, 62, 126, 1.0),
+                    backgroundColor: const Color.fromRGBO(108, 62, 126, 1.0),
                     child: Transform.rotate(
                       angle: 45 * math.pi / 180,
                       child: const IconButton(
@@ -121,8 +117,7 @@ class _MapPageState extends State<MapPage> {
           ),
         ),
       ),
-      bottomNavigationBar:
-      Stack(alignment: Alignment.bottomCenter, children: [
+      bottomNavigationBar: Stack(alignment: Alignment.bottomCenter, children: [
         SvgPicture.asset(
           "assets/images/test_navbar_without_shadow.svg",
           //color: Colors.cyan,
