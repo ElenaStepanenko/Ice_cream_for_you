0cf3154b58efadf70f0f47aa9fcc23b583d57675
Energy00 <newenglishmen.98@gmail.com>
2023-04-05 17:03:41 +0700

    1

--- android/app/src/main/kotlin/com/example/project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/project/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.project
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity: FlutterActivity() {
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    MapKitFactory.setApiKey("c4335be4-c458-4b7d-bded-424e321acd7e") // Your generated API key
+    super.configureFlutterEngine(flutterEngine)
+    }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import YandexMapsMobile
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    YMKMapKit.setApiKey("c4335be4-c458-4b7d-bded-424e321acd7e")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
