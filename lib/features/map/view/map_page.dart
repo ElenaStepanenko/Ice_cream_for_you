cdb530ec65d2be3a2279a554310eb85dc910d568
Energy00 <newenglishmen.98@gmail.com>
2023-04-05 14:48:06 +0700

    1

--- lib/features/map/view/map_page.dart
+++ lib/features/map/view/map_page.dart
@@ -13,46 +13,49 @@ class _MapPageState extends State<MapPage> {
     return Scaffold(
       body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                        width: 270,
-                        //color: Colors.white,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(30.0)
-                        ),
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                width: 270,
+                //color: Colors.white,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30.0)),
 
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.search_rounded, size: 30.0,),
-                            hintText: "Поиск",
-                            //fillColor: Colors.white,
-                            //filled: true,
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const CircleAvatar(
-                        radius: 30.0,
-                        backgroundColor: Color.fromRGBO(108, 62, 126, 1.0),
-                        child: IconButton(
-                          icon: Icon(Icons.favorite, size: 30.0, color: Color.fromRGBO(255, 255, 255, 1.0),),
-                          onPressed: null,
-                        ),
-                      )
-                    ])
-              ],
-            ),
-          )
-      ),,
+                child: const TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.search_rounded,
+                      size: 30.0,
+                    ),
+                    hintText: "Поиск",
+                    //fillColor: Colors.white,
+                    //filled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+              const CircleAvatar(
+                radius: 30.0,
+                backgroundColor: Color.fromRGBO(108, 62, 126, 1.0),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.favorite,
+                    size: 30.0,
+                    color: Color.fromRGBO(255, 255, 255, 1.0),
+                  ),
+                  onPressed: null,
+                ),
+              )
+            ])
+          ],
+        ),
+      )),
     );
   }
 }
