db43cb9719f911daff6e8d8169a1abbaecca9a70
Energy00 <newenglishmen.98@gmail.com>
2023-04-06 22:42:44 +0700

    someChanges

--- lib/features/ice_cream_list/ice_cream.dart
+++ lib/features/ice_cream_list/ice_cream.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+
 export 'view/view.dart';
+
+class IceCream {
+  String name;
+  Image photo;
+  String price;
+
+  IceCream(this.name, this.photo, this.price);
+}
