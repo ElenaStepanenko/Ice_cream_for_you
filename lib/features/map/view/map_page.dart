2bde1e3dc36591c8d35ab4f0b2b10c2b8e4057b2
Your Name <you@example.com>
2023-04-06 11:15:47 +0700

    yandex map

--- lib/features/map/view/map_page.dart
+++ lib/features/map/view/map_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'dart:math' as math;
+
+import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -11,145 +15,171 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      backgroundColor: Colors.white,
+      //persistentFooterAlignment: Per,
+      backgroundColor: const Color.fromRGBO(252, 252, 247, 1.0),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Stack(
             children: [
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: 15.0),
-                      width: 270,
-                      //color: Colors.white,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.4),
-                            spreadRadius: 5,
-                            blurRadius: 30,
-                            offset: const Offset(
-                                0, 10), // changes position of shadow
-                          ),
-                        ],
-                      ),
+              //const YandexMap(),
+              Image.asset("assets/images/map.png",
+                alignment: Alignment.topCenter,
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 15.0),
+                              width: 270,
+                              //color: Colors.white,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(30.0),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.4),
+                                    spreadRadius: 5,
+                                    blurRadius: 30,
+                                    offset: const Offset(
+                                        0, 10), // changes position of shadow
+                                  ),
+                                ],
+                              ),
 
-                      child: const TextField(
-                        decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.search_rounded,
-                            size: 30.0,
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                  icon: Icon(
+                                    Icons.search_rounded,
+                                    size: 30.0,
+                                  ),
+                                  hintText: "Поиск",
+                                  //fillColor: Colors.white,
+                                  //filled: true,
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30.0),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 16,
+                                    offset: const Offset(
+                                        0, 10), // changes position of shadow
+                                  ),
+                                ],
+                              ),
+                              child: const CircleAvatar(
+                                radius: 30.0,
+                                backgroundColor:
+                                    Color.fromRGBO(108, 62, 126, 1.0),
+                                child: IconButton(
+                                  icon: Icon(
+                                    Icons.favorite,
+                                    size: 30.0,
+                                    color: Color.fromRGBO(255, 255, 255, 1.0),
+                                  ),
+                                  onPressed: null,
+                                ),
+                              ),
+                            )
+                          ]),
+                      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 5,
+                                blurRadius: 16,
+                                offset: const Offset(
+                                    0, 10), // changes position of shadow
+                              ),
+                            ],
                           ),
-                          hintText: "Поиск",
-                          //fillColor: Colors.white,
-                          //filled: true,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
+                          child: CircleAvatar(
+                            radius: 30.0,
+                            backgroundColor:
+                                const Color.fromRGBO(108, 62, 126, 1.0),
+                            child: Transform.rotate(
+                              angle: 45 * math.pi / 180,
+                              child: const IconButton(
+                                alignment: Alignment.center,
+                                icon: Icon(
+                                  Icons.navigation_rounded,
+                                  size: 30.0,
+                                  color: Color.fromRGBO(255, 255, 255, 1.0),
+                                ),
+                                onPressed: null,
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 16,
-                            offset: const Offset(
-                                0, 10), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: const CircleAvatar(
-                        radius: 30.0,
-                        backgroundColor:
-                            Color.fromRGBO(108, 62, 126, 1.0),
-                        child: IconButton(
-                          icon: Icon(
-                            Icons.favorite,
-                            size: 30.0,
-                            color: Color.fromRGBO(255, 255, 255, 1.0),
-                          ),
-                          onPressed: null,
-                        ),
-                      ),
-                    )
-                  ]),
-              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 16,
-                        offset: const Offset(
-                            0, 10), // changes position of shadow
-                      ),
+                        )
+                      ]),
                     ],
                   ),
-                  child: CircleAvatar(
-                    radius: 30.0,
-                    backgroundColor:
-                        const Color.fromRGBO(108, 62, 126, 1.0),
-                    child: Transform.rotate(
-                      angle: 45 * math.pi / 180,
-                      child: const IconButton(
-                        alignment: Alignment.center,
-                        icon: Icon(
-                          Icons.navigation_rounded,
-                          size: 30.0,
-                          color: Color.fromRGBO(255, 255, 255, 1.0),
-                        ),
-                        onPressed: null,
-                      ),
-                    ),
-                  ),
-                )
-              ]),
-            ],
-          ),
+            ),
+          ]),
         ),
       ),
       bottomNavigationBar:
-      Stack(alignment: Alignment.bottomCenter, children: [
-        SvgPicture.asset(
-          "assets/images/test_navbar_without_shadow.svg",
-          //color: Colors.cyan,
-          width: MediaQuery.of(context).size.width,
-          fit: BoxFit.cover,
-          //height: MediaQuery.of(context).size.height,
-          //fit: BoxFit.fill,
-        ),
-        BottomNavigationBar(
-          elevation: 0,
-          backgroundColor: const Color.fromRGBO(255, 255, 255, 0.0),
-          items: [
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/images/nav_menu.svg"),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/images/nav_menu.svg"),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: SvgPicture.asset("assets/images/nav_menu.svg"),
-              label: '',
-            ),
-          ],
-        ),
-      ]),
+      Container(
+        decoration: const BoxDecoration(
+          boxShadow: [BoxShadow(
+            color: Colors.black26,
+            blurRadius: 20.0,
+            spreadRadius: 7.0,
+            offset: Offset(0, -5),
+          ),
+        ]),
+        child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+          SvgPicture.asset(
+            "assets/images/navbar_without_shadow.svg",
+            //color: Colors.cyan,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+            //height: MediaQuery.of(context).size.height,
+            //fit: BoxFit.fill,
+          ),
+          BottomNavigationBar(
+            elevation: 0,
+            backgroundColor: const Color.fromRGBO(255, 255, 255, 0.0),
+            items: [
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset("assets/images/nav_menu.svg"),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset("assets/images/nav_ice_cream.svg"),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset("assets/images/nav_menu.svg"),
+                label: '',
+              ),
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
