d230db01b9ffcf4e50b9b19d25e4860b556550a9
Energy00 <newenglishmen.98@gmail.com>
2023-04-06 12:38:20 +0700

    add route for shop

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:project/features/map/map.dart';
 import 'package:project/features/start_page/start_page.dart';
 import 'package:project/features/shops/shop.dart';
@@ -7,5 +5,5 @@ import 'package:project/features/shops/shop.dart';
 final routes = {
   '/': (context) => const StartPage(),
   '/map_page': (context) => const MapPage(),
-  '/shop_page': (context) => const ShopsPage(),
+  // '/shop_page': (context) => const ShopsPage(),
 };
