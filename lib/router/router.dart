bc5946dcac1794bfecc928124922987533860f31
Energy00 <newenglishmen.98@gmail.com>
2023-04-06 12:32:47 +0700

    add route for shop

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,11 @@
+import 'dart:js';
+
 import 'package:project/features/map/map.dart';
 import 'package:project/features/start_page/start_page.dart';
+import 'package:project/features/shops/shop.dart';
 
 final routes = {
   '/': (context) => const StartPage(),
-  '/map_page': (context) => const MapPage()
+  '/map_page': (context) => const MapPage(),
+  '/shop_page': (context) => const ShopsPage(),
 };
