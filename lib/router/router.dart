e754346ccdae22e830a30d1012babfdbb563133f
Energy00 <newenglishmen.98@gmail.com>
2023-04-14 20:39:36 +0700

    noFirstScreen

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,11 +1,11 @@
 import 'package:project/features/map/map.dart';
-import 'package:project/features/start_page/start_page.dart';
+// import 'package:project/features/start_page/start_page.dart';
 import 'package:project/features/shops/shop.dart';
 import 'package:project/features/ice_cream_list/ice_cream.dart';
 import 'package:project/features/ice_cream_page/ice_cream_p.dart';
 
 final routes = {
-  '/': (context) => const StartPage(),
+  // '/': (context) => const StartPage(),
   '/map_page': (context) => const MapPage(),
   '/shop_page': (context) => const ShopsPage(),
   '/ice_cream_list': (context) => const IceCreamsPage(),
